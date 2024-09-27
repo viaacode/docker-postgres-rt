8b3220b1f8b1d18cf159f1c11a7bf2003670dc1e
Herwig Bogaeert <herwig.bogaert@meemoo.be>
2024-09-27 16:31:58 +0200

    run consistency checks in read-only

--- recover.sh
+++ recover.sh
@@ -52,14 +52,21 @@ EOF
 
     [ -e $PGDATA/pg_ident.conf ] || touch $PGDATA/pg_ident.conf
     cp /usr/share/postgresql/postgresql.conf.sample $PGDATA/postgresql.conf
-    sed -ri "s/#?\\s*(hot_standby\\s*=)[^#]*/\1 $HOTSTANDBY /" $PGDATA/postgresql.conf
+    # Disable autovacuuum and statistics collection
+    sed -ri 's/#?\s*(autovacuum\s*=)[^#]*/\1 off/' $PGDATA/postgresql.conf
+    sed -ri 's/#?\s*(track_activities\s*=)[^#]*/\1 off/' $PGDATA/postgresql.conf
+    sed -ri 's/#?\s*(track_counts\s*=)[^#]*/\1 off/' $PGDATA/postgresql.conf
+    # Take 2 thirds of our memory limit for shared buffers
+    MemoryLimit=$(cat /sys/fs/cgroup/memory.max)
+    [ -n "$MemoryLimit" ] && sed -ri "s/#?\s*(shared_buffers\s*=)[^#]*/\1 $((MemoryLimit*2/3))B/" $PGDATA/postgresql.conf
     sed -ri 's/#?\s*(max_connections\s*=)[^#]*/\1 2000 /' $PGDATA/postgresql.conf
     sed -ri 's/#?\s*(max_standby_archive_delay\s*=)[^#]*/\1 -1 /' $PGDATA/postgresql.conf
 
     # Set recovery configuration
+    # Start database in read/only mode until consistency checks have completed
     if [ $PG_MAJOR -lt 12 ] ; then
         cat <<EOF >$PGDATA/recovery.conf
-        standby_mode=$HOTSTANDBY
+        standby_mode=on
         restore_command='echo ''{"client": "$HOSTNAME", "path": "$SRCXLOGDIR/%f", "uid": "$PGUID", "time": "$Time"}'' | socat -,ignoreeof $RecoverySocket; mv $RecoveryArea/%f $PGDATA/%p'
 EOF
         [ "$Time" != "null" ] && echo "recovery_target_time='$Time'" >>$PGDATA/recovery.conf
@@ -67,7 +74,7 @@ EOF
         cat <<-EOF >>$PGDATA/postgresql.conf
         restore_command='echo ''{"client": "$HOSTNAME", "path": "$SRCXLOGDIR/%f", "uid": "$PGUID", "time": "$Time"}'' | socat -,ignoreeof $RecoverySocket; mv $RecoveryArea/%f $PGDATA/%p'
 EOF
-        [ $HOTSTANDBY == "on" ] &&  touch $PGDATA/standby.signal || touch $PGDATA/recovery.signal
+        touch $PGDATA/standby.signal
         [ "$Time" != "null" ] && echo "recovery_target_time='$Time'" >>$PGDATA/postgresql.conf
     fi
 
@@ -86,7 +93,9 @@ EOF
     # Show progress while waiting untill recovery is complete
     while read -ru ${tailcop[0]} line; do
         echo $line
+	# Break when recovery is complete:
         [ $(expr "$line" : '.*LOG:\s*database system is ready to accept .*connections') -gt 0 ] && break
+	# Extract certain log entries for the recovery report
         [ $(expr "$line" : '.*LOG:\s*redo') -gt 0 ] && echo $line >>$REPORT
         [ $(expr "$line" : '.*LOG:\s*last completed transaction was at log time') -gt 0 ] && echo $line >>$REPORT
         [ $(expr "$line" : '.*LOG:\s*consistent recovery state reached') -gt 0 ] && echo $line >>$REPORT
@@ -97,17 +106,22 @@ EOF
     [ $? -ne 0 ] && echo "$(date '+%m/%d %H:%M:%S'): Database recovery failed" | tee -a $REPORT && exit 1
 
     # continue reading and showing stdout of the coprocess
+    # redirecting from ${tailcop[0]} directly to cat does not work, use exec
     exec 3<&${tailcop[0]}
     cat <&3 &
-
+    # Stop searching for wal's to apply (until consistency check has completed)
+    psql -qAc "select pg_wal_replay_pause();"
+    # Report recovery timestamp
     psql -qAtc "select 'Last replay timestamp: ' || pg_last_xact_replay_timestamp();" | tee -a $REPORT
+
     echo "$(date '+%m/%d %H:%M:%S'): Checking database integrity"
-    [ $HOTSTANDBY == 'on' ] &&  psql -qc  "select pg_wal_replay_pause();"
     pg_dumpall -v --no-sync -f /dev/null
     RC=$? # save rc
-    [ $HOTSTANDBY == 'on' ] &&  psql -qc  "select pg_wal_replay_resume();"
     echo "$(date '+%m/%d %H:%M:%S'): Database integrity check endend with exit code $RC" | tee -a $REPORT
     [ $RC -ne 0 ] && echo "$(date '+%m/%d %H:%M:%S'): Database integrity check failed" && exit $RC
+
+    # Leave temporary read-only mode when hotstandby is not requested
+    [ $HOTSTANDBY != 'on' ] && pg_ctl promote
 
     echo "$(date '+%m/%d %H:%M:%S'): Shutting down postgres"
     # Stop the coprocess and wait for it to shutdown
