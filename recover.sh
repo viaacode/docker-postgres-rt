16cb09a71790d7c06de69f8b446505f7c4c384dc
Herwig Bogaert <herwig.bogaert@meemoo.be>
2024-11-27 09:55:18 +0100

    allow specification of rpo

--- recover.sh
+++ recover.sh
@@ -24,7 +24,7 @@ if [ ! -s "$PGDATA/PG_VERSION" ]; then
     [ -n "$SRCDATADIR" ] || exit 2
     SRCXLOGDIR=${SRCXLOGDIR:=$SRCDATADIR/pg_wal}
     Time=${Time:=null}
-    
+
     PGDATADIR="$RecoveryArea/$(basename $SRCDATADIR)"
     PGUID=$(id -u postgres)
     REPORT="$RecoveryArea/recovery_report.txt"
@@ -58,7 +58,7 @@ EOF
     sed -ri 's/#?\s*(track_counts\s*=)[^#]*/\1 off/' $PGDATA/postgresql.conf
     # Take 2 thirds of our memory limit for shared buffers
     MemoryLimit=$(cat /sys/fs/cgroup/memory.max)
-    [ -n "$MemoryLimit" ] && sed -ri "s/#?\s*(shared_buffers\s*=)[^#]*/\1 $((MemoryLimit*2/3))B/" $PGDATA/postgresql.conf
+    [ -n "$MemoryLimit" ] && sed -ri "s/#?\s*(shared_buffers\s*=)[^#]*/\1 $((MemoryLimit/4096))kB/" $PGDATA/postgresql.conf
     sed -ri 's/#?\s*(max_connections\s*=)[^#]*/\1 2000 /' $PGDATA/postgresql.conf
     sed -ri 's/#?\s*(max_standby_archive_delay\s*=)[^#]*/\1 -1 /' $PGDATA/postgresql.conf
 
@@ -70,12 +70,23 @@ EOF
 	ConfFile=postgresql.conf
         touch $PGDATA/standby.signal
     fi
+   if [ "$Time" != "null" ]; then
+	# When recovery_target_time is given, postgres may need to examine wal records that have been
+	# archived later then the given timestamp in order to find the first commit after the timestamp
+	# given. Therfore we shift the time window in which we are looking for backups for 4 hours.
+        RPOEpoch=$(date -d "$Time" +%s)
+        WalTimeEpoch=$((RPOEpoch + 4*3600))
+        WalTime=$(date -d @$WalTimeEpoch -Ins)
+    else
+        WalTime="$Time"
+    fi
     cat <<EOF >>$PGDATA/$ConfFile
-    restore_command='echo ''{"client": "$HOSTNAME", "path": "$SRCXLOGDIR/%f", "uid": "$PGUID", "time": "$Time"}'' | socat -,ignoreeof $RecoverySocket; mv $RecoveryArea/%f $PGDATA/%p'
+    restore_command='echo ''{"client": "$HOSTNAME", "path": "$SRCXLOGDIR/%f", "uid": "$PGUID", "time": "$WalTime"}'' | socat -,ignoreeof $RecoverySocket; mv $RecoveryArea/%f $PGDATA/%p'
 EOF
     if [ "$Time" != "null" ]; then
         echo "recovery_target_time='$Time'" >>$PGDATA/$ConfFile
     else
+	# If $Time is not set, we recover until consistent
         echo "recovery_target = 'immediate'" >>$PGDATA/$ConfFile
     fi
     echo "host all all samenet trust" > "$PGDATA/pg_hba.conf"
@@ -85,14 +96,13 @@ EOF
     cat $PGDATADIR/backup_label 2>&1 | tee -a $REPORT
     echo "$(date '+%m/%d %H:%M:%S'): Starting postgres recovery (hot_standby = $HOTSTANDBY)"
 
-    echo "$(date '+%m/%d %H:%M:%S'): Starting postgres recovery"
     # Start postgres without listening on a tcp socket
     coproc tailcop { exec docker-entrypoint.sh -h '' 2>&1; }
 
     # Show progress while waiting untill recovery is complete
     while read -ru ${tailcop[0]} line; do
         echo $line
-	# Break when recovery is complete:
+	# Break when consistent recovery state is reached
         [ $(expr "$line" : '.*LOG:\s*database system is ready to accept .*connections') -gt 0 ] && break
 	# Extract certain log entries for the recovery report
         [ $(expr "$line" : '.*LOG:\s*redo') -gt 0 ] && echo $line >>$REPORT
@@ -104,6 +114,14 @@ EOF
     # for example, postgres stopped
     [ $? -ne 0 ] && echo "$(date '+%m/%d %H:%M:%S'): Database recovery failed" | tee -a $REPORT && exit 1
 
+    # If $Time is set, we need to allow time for recovery to continue until the recovery_target is reached
+    if [ "$Time" != "null" ]; then
+      while read -ru ${tailcop[0]} line; do
+        echo $line
+	# Break when recovery is complete:
+        [ $(expr "$line" : '.*LOG:\s*recovery stopping ') -gt 0 ] && break
+      done
+    fi
     # continue reading and showing stdout of the coprocess
     # redirecting from ${tailcop[0]} directly to cat does not work, use exec
     exec 3<&${tailcop[0]}
@@ -112,9 +130,6 @@ EOF
     psql -qAtc "select 'Last replay timestamp: ' || pg_last_xact_replay_timestamp();" | tee -a $REPORT
 
     echo "$(date '+%m/%d %H:%M:%S'): Checking database integrity"
-    # Stop searching for wal's to apply (until consistency check has completed)
-    # Only needed when recovery_target is not immediate
-    [ "$Time" != "null" ] && psql -qAc "select pg_wal_replay_pause();"
     pg_dumpall -v --no-sync -f /dev/null
     RC=$? # save rc
     echo "$(date '+%m/%d %H:%M:%S'): Database integrity check endend with exit code $RC" | tee -a $REPORT
