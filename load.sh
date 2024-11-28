5e2a44be35686e4be1b6b5a3282b20ac130af99b
Herwig Bogaert <herwig.bogaert@meemoo.be>
2024-11-28 16:21:42 +0100

    disable autovacuum and set memory also for load

--- load.sh
+++ load.sh
@@ -31,9 +31,17 @@ EOF
     [ -r $DUMPFILE ] || exit 5
     ln -s "$DUMPFILE" "/docker-entrypoint-initdb.d/10-$DumpBaseName"
     echo "$(date '+%m/%d %H:%M:%S'): Starting postgres init"
+    cp /usr/share/postgresql/postgresql.conf.sample /var/lib/postgresql/postgresql.conf
+    # Disable autovacuuum and statistics collection
+    sed -ri 's/#?\s*(autovacuum\s*=)[^#]*/\1 off/' /var/lib/postgresql/postgresql.conf
+    sed -ri 's/#?\s*(track_activities\s*=)[^#]*/\1 off/' /var/lib/postgresql/postgresql.conf
+    sed -ri 's/#?\s*(track_counts\s*=)[^#]*/\1 off/' /var/lib/postgresql/postgresql.conf
+    # Take 2 thirds of our memory limit for shared buffers
+    MemoryLimit=$(cat /sys/fs/cgroup/memory.max)
+    [ -n "$MemoryLimit" ] && sed -ri "s/#?\s*(shared_buffers\s*=)[^#]*/\1 $((MemoryLimit/4096))kB/" /var/lib/postgresql/postgresql.conf
     # Start postgres without listening on a tcp socket
     export POSTGRES_PASSWORD="$RecoverySecret"
-    coproc tailcop { exec docker-entrypoint.sh -h '' 2>&1; }
+    coproc tailcop { exec docker-entrypoint.sh -h '' -c 'config_file=/var/lib/postgresql/postgresql.conf' 2>&1; }
 
     # initiate a timeout killer that will stop popstgres (and the container) if init takes too long
     sleep 10800 && echo "$(date '+%m/%d %H:%M:%S'): Timeout during init" && kill $tailcop_PID &
@@ -58,5 +66,7 @@ EOF
     echo "$(date '+%m/%d %H:%M:%S'): Shutting down postgres"
     # Stop the coprocess and show it's output while waiting for it to stop
     [ -n "$tailcop_PID" ] && kill $tailcop_PID && wait $tailcop_PID
+    # Copy our custom config file to the default config
+    cp /var/lib/postgresql/postgresql.conf $PGDATA/postgresql.conf
 fi
 exec docker-entrypoint.sh postgres
